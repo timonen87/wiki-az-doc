4ee34c9a3895fff2ce87ee0c3c9a9c0bccdf1ed5
timonen87 <timonen.as@gmail.com>
2024-10-24 18:00:13 +0300

    add fix docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,54 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'WIKI AZ',
-  tagline: 'База знаний компании АZ',
-  favicon: 'img/favicon.ico',
+  title: "WIKI AZ",
+  tagline: "База знаний компании АZ",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://wikiaz.ru',
+  url: "https://wikiaz.ru",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'AZ', // Usually your GitHub org/user name.
-  projectName: 'AZ', // Usually your repo name.
+  organizationName: "AZ", // Usually your GitHub org/user name.
+  projectName: "AZ", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'ru',
-    locales: ['ru'],
+    defaultLocale: "ru",
+    locales: ["ru"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/timonen87/wiki-az-doc/tree/main/frontend',
+            "https://github.com/timonen87/wiki-az-doc/tree/main/frontend",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/timonen87/wiki-az-doc/tree/main/frontend',
+            "https://github.com/timonen87/wiki-az-doc/tree/main/frontend",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -56,63 +56,61 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'WIKI AZ',
+      title: "WIKI AZ",
       logo: {
-        alt:'WIKI AZ Logo',
-        src: 'img/logo.svg',
+        alt: "WIKI AZ Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'База знаний',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "База знаний",
         },
         // {to: '/blog', label: 'Блог', position: 'left'},
         {
-          "to": "https://wikiapp.ru",
-          "label": "WikiApp",
-          "position": "left"
+          to: "https://wikiapp.ru",
+          label: "WikiApp",
+          position: "left",
         },
         {
-          href: 'https://github.com/timonen87/wiki-az-doc/tree/main/frontend',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/timonen87/wiki-az-doc/tree/main/frontend",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Документиция',
+          title: "Документиция",
           items: [
             {
-              label: 'База знаний',
-              to: '/docs/intro',
+              label: "База знаний",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Каталог продукции',
-          "items": [
+          title: "Каталог продукции",
+          items: [
             {
-              "label": "WikiApp",
-              "href": "https://wikiapp.ru/"
+              label: "WikiApp",
+              href: "https://wikiapp.ru/",
             },
- 
           ],
         },
         {
-          title: 'Разное',
-          "items": [
-          
+          title: "Разное",
+          items: [
             {
-              "label": "GitHub",
-              "href": "https://github.com/timonen87/wiki-az-doc/"
-            }
+              label: "GitHub",
+              href: "https://github.com/timonen87/wiki-az-doc/",
+            },
           ],
         },
       ],
