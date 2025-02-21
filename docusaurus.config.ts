3dca3dd550f28eb8b1fc9ddfc819016590334ade
timonen87 <timonen.as@gmail.com>
2025-02-21 14:59:21 +0300

    add fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://wikiaz.ru",
+  url: "https://docs.azcompany.ru",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
